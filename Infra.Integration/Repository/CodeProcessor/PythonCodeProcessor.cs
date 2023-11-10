758cc68038ab0e48185bbd3097aae93f130ca5dd
solange4 <sromeroc@unsa.edu.pe>
2023-11-10 14:47:09 -0500

    feat: add PythonCodeProcessor > checkSyntax

--- Infra.Integration/Repository/CodeProcessor/PythonCodeProcessor.cs
+++ Infra.Integration/Repository/CodeProcessor/PythonCodeProcessor.cs
@@ -1,5 +1,11 @@
-﻿using Domain.Application.Contracts;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Domain.Application.Contracts;
 using Domain.Core.Models;
+using IronPython.Hosting;
+using Microsoft.Scripting;
+using Microsoft.Scripting.Hosting;
 
 namespace Infra.Integration.Repository.CodeProcessor
 {
@@ -7,13 +13,34 @@ namespace Infra.Integration.Repository.CodeProcessor
     {
         public SyntaxStatus checkSyntax(string code)
         {
-            throw new NotImplementedException();
+            var result = new SyntaxStatus();
+
+            try
+            {
+                var engine = Python.CreateEngine();
+                var scriptSource = engine.CreateScriptSourceFromString(code, SourceCodeKind.Statements);
+                scriptSource.Compile();
+
+                result.IsOk = true;
+            }
+            catch (SyntaxErrorException ex)
+            {
+                result.IsOk = false;
+                result.ErrorMsg.Add($"Syntax error in line {ex.Line}: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                result.IsOk = false;
+                result.ErrorMsg.Add($"Error: {ex.Message}");
+            }
+
+            return result;
         }
 
 
         public async Task<SyntaxStatus> CheckSyntaxAsync(string codeToCheck)
         {
-            throw new NotImplementedException();
+            return checkSyntax(codeToCheck);
         }
 
         public SyntaxStatus compile(string code)
