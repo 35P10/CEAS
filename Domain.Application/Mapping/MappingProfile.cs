173cf730df4b86ae0b34ffb191a54f2bc67b6eb3
MaryoriHilares <mhilaresa@unsa.edu.pe>
2023-11-10 13:24:34 -0500

    Mapping

--- Domain.Application/Mapping/MappingProfile.cs
+++ Domain.Application/Mapping/MappingProfile.cs
@@ -14,7 +14,13 @@ namespace Externos.Domain.Application.Mappings
                 .ForMember(dt => dt.ErrorMsg, opt => opt.MapFrom(src => src.ErrorMsg))
                 .ForMember(dt => dt.Obs, opt => opt.MapFrom(src => src.ObsMsg.Count))
                 .ForMember(dt => dt.ObsMsg, opt => opt.MapFrom(src => src.ObsMsg));
-            CreateMap<SyntaxStatus,RunResponseVM>();
+            
+            CreateMap<SyntaxStatus,RunResponseVM>()
+                .ForMember(dt => dt.IsOk, opt => opt.MapFrom(src => src.IsOk))
+                .ForMember(dt => dt.Errors, opt => opt.MapFrom(src => src.ErrorMsg.Count))
+                .ForMember(dt => dt.ErrorMsg, opt => opt.MapFrom(src => src.ErrorMsg))
+                .ForMember(dt => dt.Obs, opt => opt.MapFrom(src => src.ObsMsg.Count))
+                .ForMember(dt => dt.ObsMsg, opt => opt.MapFrom(src => src.ObsMsg));
         }
     }
 }
--- Domain.Application/Models/RunResponseVM.cs
+++ Domain.Application/Models/RunResponseVM.cs
@@ -4,5 +4,10 @@
     {
         public int IdResponse { get; set; } = 0;
         public string? Output { get; set; } = "";
+        public bool IsOk {get; set;}
+        public int Errors {get; set;} = 0;
+        public int Obs {get; set;} = 0;
+        public List<string> ErrorMsg {get; set;}
+        public List<string> ObsMsg {get; set;}
     }
 }
