d2b113322a0515171cec887dfa62116ccbb10339
villanueva <villanueva.guerrero@outlook.com>
2023-11-10 12:30:40 -0500

    Test: add Python CheckSyntax use cases

---
+++ Tests/CEAS.Infra.Integration/Repository/PythonCodeProcessorTest.cs
@@ -0,0 +1,39 @@
+﻿using Xunit;
+using Infra.Integration.Repository.CodeProcessor;
+
+namespace CEAS.Infra.Integration.Test.Repository
+{
+    public class PythonCodeProcessorTest
+    {
+        PythonCodeProcessor _PythonCodeProcessor;
+        public PythonCodeProcessorTest()
+        {
+            _PythonCodeProcessor = new PythonCodeProcessor();
+        }
+
+        [Fact]
+        public async Task CheckSyntax_ValidCode()
+        {
+            string codeToCheck = "print('Hello, World!')";
+
+            var result = await _PythonCodeProcessor.CheckSyntaxAsync(codeToCheck);
+
+            Assert.True(result.IsOk);
+            Assert.Empty(result.ErrorMsg);
+        }
+
+        [Fact]
+        public async Task CheckSyntax_InvalidCode()
+        {
+            string codeToCheck = "print('Hello, World'";
+
+            var result = await _PythonCodeProcessor.CheckSyntaxAsync(codeToCheck);
+
+            Assert.False(result.IsOk);
+            Assert.NotEmpty(result.ErrorMsg);
+        }
+
+    }
+
+}
+
