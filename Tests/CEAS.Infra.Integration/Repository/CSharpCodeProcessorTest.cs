e5698b2f2be00d1d016ca8a79bb2e302767c4ede
villanueva <villanueva.guerrero@outlook.com>
2023-11-10 14:55:01 -0500

    test: add tests for python and csharp

--- Tests/CEAS.Infra.Integration/Repository/CSharpCodeProcessorTest.cs
+++ Tests/CEAS.Infra.Integration/Repository/CSharpCodeProcessorTest.cs
@@ -32,6 +32,40 @@ namespace CEAS.Infra.Integration.Test.Repository
             Assert.NotEmpty(result.ErrorMsg);
         }
 
+        [Fact]
+        public async Task CheckSyntax_EmptyCode()
+        {
+            string codeToCheck = "";
+            var result = await _cSharpCodeProcessor.CheckSyntaxAsync(codeToCheck);
+
+            Assert.True(result.IsOk);
+            Assert.Empty(result.ErrorMsg);
+        }
+
+        [Fact]
+        public async Task CheckSyntax_CodeWithWarning()
+        {
+            string codeToCheck = "using System; class MyClass { static void Main() { Console.WriteLine(\"Hello, World!\"); } }";
+
+            var result = await _cSharpCodeProcessor.CheckSyntaxAsync(codeToCheck);
+
+            Assert.True(result.IsOk);
+            Assert.Empty(result.ErrorMsg);
+            Assert.NotEmpty(result.ObsMsg); // Asegura que hay observaciones (advertencias) en el resultado
+        }
+
+        [Fact]
+        public async Task CheckSyntax_CodeWithMultipleErrors()
+        {
+            string codeToCheck = "using System; class MyClass { static void Main() { Console.WriteLine(\"Hello, World!\") }";
+
+            var result = await _cSharpCodeProcessor.CheckSyntaxAsync(codeToCheck);
+
+            Assert.False(result.IsOk);
+            Assert.NotEmpty(result.ErrorMsg);
+            Assert.True(result.ErrorMsg.Count > 1); // Asegura que hay más de un error en el resultado
+        }
+
     }
 
 }
