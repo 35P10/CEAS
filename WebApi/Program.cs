c63367720bad8aee6183101f36519aec4fe394a1
villanueva <villanueva.guerrero@outlook.com>
2023-11-14 09:37:39 -0500

    build: set CORN policy for all origins

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -8,6 +8,16 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddCors(options =>
+                    {
+                        options.AddPolicy("AllowAnyOrigin",
+                            builder => builder.AllowAnyOrigin()
+                                            .AllowAnyMethod()
+                                            .AllowAnyHeader());
+                    });
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -28,6 +38,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseCors("AllowAnyOrigin");
 
 app.MapControllers();
 
